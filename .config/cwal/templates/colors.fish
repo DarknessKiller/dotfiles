625a642459fd4bdb7fe3a08ab6feb599c6d32bf3
DarknessKiller <mrdarknesskiller@protonmail.com>
2026-06-24 03:06:49 +0800

    refactor(template): move fzf and ls colours

--- .config/cwal/templates/colors.fish
+++ .config/cwal/templates/colors.fish
@@ -37,3 +37,13 @@ set fish_pager_color_selected_background --background={color8.strip}
 set fish_pager_color_selected_completion {foreground.strip}
 set fish_pager_color_selected_description {color8.strip}
 set fish_pager_color_selected_prefix {color10.strip}
+
+# FZF colors
+export FZF_DEFAULT_OPTS="
+    $FZF_DEFAULT_OPTS
+    --color fg:7,bg:0,hl:1,fg+:232,bg+:1,hl+:255
+    --color info:7,prompt:2,spinner:1,pointer:232,marker:1
+"
+
+# Fix LS_COLORS being unreadable.
+export LS_COLORS="$LS_COLORS:su=30;41:ow=30;42:st=30;44:"
--- .config/cwal/templates/terminal.sh
+++ .config/cwal/templates/terminal.sh
@@ -24,12 +24,3 @@ printf "\033]11;{background}\033\\"   # background
 printf "\033]12;{cursor}\033\\"       # cursor
 printf "\033]708;{border}\033\\"      # border
 
-# FZF colors
-export FZF_DEFAULT_OPTS="
-    $FZF_DEFAULT_OPTS
-    --color fg:7,bg:0,hl:1,fg+:232,bg+:1,hl+:255
-    --color info:7,prompt:2,spinner:1,pointer:232,marker:1
-"
-
-# Fix LS_COLORS being unreadable.
-export LS_COLORS="$LS_COLORS:su=30;41:ow=30;42:st=30;44:"
