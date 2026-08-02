9b08f38117756b5573bd45ab5957abc3833f50ac
DarknessKiller <mrdarknesskiller@protonmail.com>
2026-08-02 17:03:40 +0800

    feat: add bw-sync function for generating SSH config from Bitwarden identities

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,12 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 set fish_greeting
 
+# User functions
+if test -f ~/.config/fish/functions.fish
+source ~/.config/fish/functions.fish
+end
+
+# User configuration
 if test -f ~/.fishrc
     source ~/.fishrc
 end
