f056fa8bc002ed291c74a46827541efdbfefb548
DarknessKiller <mrdarknesskiller@protonmail.com>
2026-09-20 11:42:35 +0800

    refactor(cwal): reset templates to default

--- .config/cwal/templates/colors-nvim.lua
+++ .config/cwal/templates/colors-nvim.lua
@@ -1,20 +1,19 @@
--- Catppuccin Macchiato + dynamic accents
 return {
     wallpaper = "{wallpaper}",
 
-    background = "#24273a",
+    background = "{background}",
     foreground = "{foreground}",
-    cursor = "{color4}",
+    cursor = "{cursor}",
 
-    color0 = "#181926",
+    color0 = "{color0}",
     color1 = "{color1}",
     color2 = "{color2}",
     color3 = "{color3}",
     color4 = "{color4}",
     color5 = "{color5}",
     color6 = "{color6}",
-    color7 = "{foreground}",
-    color8 = "#1e2030",
+    color7 = "{color7}",
+    color8 = "{color8}",
     color9 = "{color9}",
     color10 = "{color10}",
     color11 = "{color11}",
